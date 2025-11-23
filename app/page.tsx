5a5a56d1fb6c48ed29f6b980d347eb48b6886af7
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-11-23 13:58:51 +0530

    eslint and tailwind related configs

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     <div className="text-lg text-orange-400">Jay Shree Ram</div>
-      <Button variant="outline" size={"lg"}>Click it</Button>
+      <div className="text-lg text-orange-400">Jay Shree Ram</div>
+      <Button variant="outline" size={"lg"}>
+        Click it
+      </Button>
     </div>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,22 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+const eslintConfig = defineConfig({
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
+  extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  plugins:['prettier'],
+  rules:{
+    'prettier/prettier': 'error',
+    'react/no-escape-entities': 'off',
+  },
+  ...globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
-]);
+});
 
 export default eslintConfig;
