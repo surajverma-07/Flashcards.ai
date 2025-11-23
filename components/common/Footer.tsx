271ad58a330a0e2d7f1714a20cb68a8323b182c8
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-11-23 15:24:50 +0530

    Footer Created

---
+++ components/common/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+   return(
+      <footer className="w-full border-t bg-zinc-200 dark:bg-zinc-900 border-zinc-300 dark:border-zinc-700 py-6 text-center text-sm text-zinc-600 dark:text-zinc-400">
+        &copy; 2025 FlashcardAI. All rights reserved.
+      </footer>
+   )
+}
