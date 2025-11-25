373845d533d3bc66ed50ab4a6c40042ae19f497d
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2025-11-25 22:58:49 +0530

    Header is fixed

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,7 +2,7 @@ import {CardSim} from 'lucide-react';
 import NavLink from './NavLink'
 const Header = () =>{
   const isLoggedIn = false;
-  return <nav className="container flex items-center justify-between py-4 px-2 lg:px-6 mx-auto sticky top-0">
+  return <nav className="container bg-zinc-50 b w-full flex items-center justify-between py-4 px-2 lg:px-6 mx-auto sticky top-0 min-w-screen">
       <div className="flex ">
           <NavLink href='/' className="flex items-center gap-1 lg:gap-2 shrink-0">
             <CardSim className="inline-block  w-6 h-6 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition duration-200 ease-in-out" />  
