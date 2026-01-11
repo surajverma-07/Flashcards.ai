9fc0f1e6955672a7a21742eca66cb9250cfb6494
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2026-01-11 13:16:35 +0530

    gradiant added in the background

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import BgGradiant from "@/components/common/BgGradiant";
 import { HeroSection } from "@/components/home/heroSection";
 
 export default function Home() {
   return (
     <section>
-       <HeroSection />
+      <div className="relative w-full">
+        <BgGradiant />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
 
+      </div>
     </section>
   );
 }
---
+++ components/common/BgGradiant.tsx
@@ -0,0 +1,25 @@
+export default function BgGradiant({
+children,
+className,
+}:{
+  children?:React.ReactNode;
+  className?:string;
+}){
+  return (
+    <div className={`${className} relative isolate`}>
+        <div 
+          aria-hidden="true"
+          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-30"
+        >
+            <div
+            style={{
+              clipPath:'polygon(50% 0%,61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)',
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125] -translate-x-1/2 rotate-30 bg-linear-to-br from-emerald-500  via-teal-500 to-cyan-500 opacity-20 sm:left-[calc(50%-30rem)] sm:w-6xl"
+            >
+            </div>
+            {children}
+        </div>
+    </div>
+  )
+}
